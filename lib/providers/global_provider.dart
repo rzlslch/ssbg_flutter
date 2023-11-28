3a58380407b3d8f1bb745614daa7b1b12f5f4c51
Rizal M. S <rizal.solichudin@gmail.com>
2023-11-28 18:12:15 +0700

    update

--- lib/providers/global_provider.dart
+++ lib/providers/global_provider.dart
@@ -62,13 +62,13 @@ class GlobalProvider with ChangeNotifier {
 
   void addPost(ListModel listModel) {
     _listPost.add(listModel);
-    _listPost.sort((b, a) => a.created.compareTo(b.created));
+    _listPost.sort((b, a) => a.filename.compareTo(b.filename));
     notifyListeners();
   }
 
   void setPost(List<ListModel> list) {
     _listPost = list;
-    _listPost.sort((b, a) => a.created.compareTo(b.created));
+    _listPost.sort((b, a) => a.filename.compareTo(b.filename));
     notifyListeners();
   }
 }
